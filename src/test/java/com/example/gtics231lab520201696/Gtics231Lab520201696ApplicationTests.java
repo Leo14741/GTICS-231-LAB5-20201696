1d8ce2c9435d53475081aea662fd8719a0f065e0
leonardo <a20201696@pucp.edu.pe>
2023-04-25 20:29:26 -0500

    Initial commit

---
+++ src/test/java/com/example/gtics231lab520201696/Gtics231Lab520201696ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gtics231lab520201696;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gtics231Lab520201696ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
